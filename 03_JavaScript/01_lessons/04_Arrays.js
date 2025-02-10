2faa88a6310242df83d80074df98ae5fa0e0ce15
Chirag Arora <cse.chirag.arora@gmail.com>
2025-02-10 19:11:23 +0530

    added 6 Array methods

--- 03_JavaScript/01_lessons/04_Arrays.js
+++ 03_JavaScript/01_lessons/04_Arrays.js
@@ -17,3 +17,32 @@ for(let i=0;i<teas.length;i++){
 }
 console.log(longestTeaName)
 console.log(teas)
+
+// Practice all array methods
+
+// Adding elements
+
+// add new element at the end
+teas.push('Orange Tea')
+// add new element at the start/beginning
+teas.unshift('Dandelion Tea')
+// in the middle
+teas.splice(3,0, 'Jasmine Tea')
+
+
+// REMOVE element
+// from the end
+teas.pop()
+// from the start
+teas.shift()
+// from middle
+teas.splice(2,1)
+
+// UPdate element
+teas[1] = 'Dandelion Tea'
+console.log(teas)
+
+// Static methods
+// forEach, map, filter, sort, reduce, from, isArray, at, concat, copyWithin, entries, every, fill, filter, find, findIndex, findLast, findLastIndex, flat, flatMap, includes, indexOf, join, keys, lastIndexOf, pop, push, reduce, reduceRight, reverse, shift, slice, some, toLocaleString, toREversed, toSorted, toSpliced, toString, unshift, values, with,
+
+console.log(Array(2,3,4,5))
