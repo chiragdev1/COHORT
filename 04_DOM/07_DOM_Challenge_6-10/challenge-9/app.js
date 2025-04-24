aa92133b3315aa91857430a1736139f8d2da9cd8
Chirag Arora <cse.chirag.arora@gmail.com>
2025-04-24 13:44:17 +0530

    Completed sliding window challenge

--- 04_DOM/07_DOM_Challenge_6-10/challenge-9/app.js
+++ 04_DOM/07_DOM_Challenge_6-10/challenge-9/app.js
@@ -1,3 +1,24 @@
-/**
- * Write your challenge solution here
- */
+const panel = document.querySelector('.panel')
+
+// Write a function openMenu that add active class to panel and closeMenu() that removes it.
+// Add eventListener to document for click, check the target to close the panel
+// Add click event to X button on the panel
+
+function openMenu() {
+   panel.classList.add('active')
+}
+function closeMenu() {
+   panel.classList.remove('active')
+}
+
+document.addEventListener('click', (e) => {
+   const target = e.target
+   if (
+      !e.target.classList.contains("panel") &&
+      !e.target.classList.contains("menu-item") &&
+      !e.target.classList.contains("toggle-btn")
+   ) {
+      closeMenu();
+   }
+   console.log(target)
+})
