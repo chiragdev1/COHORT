b5e6c85533f2072f5e3597d69c3adb372190d12d
Chirag Arora <cse.chirag.arora@gmail.com>
2025-05-28 20:40:50 +0530

    reactJs

---
+++ 08_React/05_vite/src/App.jsx
@@ -0,0 +1,10 @@
+export function App  () {
+   console.log("Running App.jsx");
+
+   return (
+      <div>
+         <h1>Welcome to DSADEN! </h1>
+         <p>Onestop solution for your DSA!</p>
+      </div>
+   )
+}
